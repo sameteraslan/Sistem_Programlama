6b8bf10f2462f422c03c0d9c38dc80484ceb8f02
sam <sam>
2019-10-11 14:39:07 +0300

    update

--- Assembly/Notlar/Hafta_03/hafta_03_lab/sumofarray.asm
+++ Assembly/Notlar/Hafta_03/hafta_03_lab/sumofarray.asm
@@ -4,30 +4,25 @@ global sumofarray
 sumofarray:
     push ebp
     mov ebp, esp
-    push ebx
-    sub esp, 4
 
-    mov eax, [ebp+12]  ;smallest will be here
-    mov ebx, [eax]
-    mov [ebp-8], ebx
-
-    mov edx, [ebp+8]
-    mov ecx, [ebp+12]
-    xor ebx, ebx
-    add ebx, [ecx]
+    mov eax, [ebp+12]
+    mov ebx, [ebp+8]
+    xor ecx,ecx
+    mov ecx, [eax]
 
 loop:
-    add ecx, 4
-    mov eax, [ecx]
-    add ebx, eax
-    dec edx
-    cmp edx, 1
+    add eax, 4
+    add ecx, [eax]
+    sub ebx, 1
+    cmp ebx, 1
     jne loop
 
-ending:
-    mov eax, ebx
+    mov eax, ecx
     mov esp, ebp
-    sub esp, 4
-    pop ebx
     pop ebp
     ret
+
+    
+    
+
+    
